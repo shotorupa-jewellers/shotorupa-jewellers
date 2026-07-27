c523d5aec8be3f5255f725ada3b58f91aa9ab1ad
shotorupa-jewellers <rjs665566@gmail.com>
2026-07-27 18:49:03 +0600

    fix shared product types

---
+++ src/types/product.ts
@@ -0,0 +1,21 @@
+export type Product = {
+
+id:string;
+
+name:string;
+
+price:number;
+
+image:string;
+
+category:string;
+
+weight:string;
+
+purity:string;
+
+metal?:string;
+
+stone?:string;
+
+};
